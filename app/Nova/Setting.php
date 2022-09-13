572d8bf4f4ee55fa53e0fd94ad1a91a9be47bbdd
Jimmy <jimmy_jra@hotmail.com>
2022-09-14 02:00:29 +0300

    ReviewPolicy and Setting resource

---
+++ app/Nova/Setting.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Image;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Number;
+use Nsavinov\NovaPercentField\Percent;
+use Epartment\NovaDependencyContainer\HasDependencies;
+use Epartment\NovaDependencyContainer\NovaDependencyContainer;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Setting extends Resource
+{
+
+    use HasDependencies;
+
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Setting::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            // ID::make(__('ID'), 'id')->sortable(),
+            Boolean::make('سماح البيع بالعمولة','referral'),
+            NovaDependencyContainer::make([
+                Percent::make('% نسبة العمولة', 'referral_percentage')->rules('required')->precision(0),
+            ])->dependsOn('referral', true),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+
+    public static function singularLabel()
+    {
+        return 'ضبط';
+    }
+
+    public static function label()
+    {
+        return 'الأعدادات';
+    }
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,18 +7,16 @@ namespace Composer\Autoload;
 class ComposerStaticInit044026184457c9850706b2f68d6e9c44
 {
     public static $files = array (
+        'ec07570ca5a812141189b1fa81503674' => __DIR__ . '/..' . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
-        'ec07570ca5a812141189b1fa81503674' => __DIR__ . '/..' . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
         '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
         'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
-        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0d59ee240a4cd96ddbb4ff164fccea4d' => __DIR__ . '/..' . '/symfony/polyfill-php73/bootstrap.php',
-        '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
-        'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '53c7be3308b5aa229d03a4dfcf6ca18d' => __DIR__ . '/..' . '/symfony/intl/Resources/functions.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
@@ -38,8 +36,10 @@ class ComposerStaticInit044026184457c9850706b2f68d6e9c44
         '56823cacd97af379eceaf82ad00b928f' => __DIR__ . '/..' . '/phpseclib/bcmath_compat/lib/bcmath.php',
         '34122c0574b76bf21c9a8db62b5b9cf3' => __DIR__ . '/..' . '/cakephp/chronos/src/carbon_compat.php',
         'ed33d19cba977f2a7e321f120d94a872' => __DIR__ . '/..' . '/spatie/once/src/functions.php',
+        '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
         '9cdd7b9056abc3081735233ba9dd9c7f' => __DIR__ . '/..' . '/facade/flare-client-php/src/helpers.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+        'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
         'ed962a97bd972bc82007176b647d4e36' => __DIR__ . '/..' . '/facade/ignition/src/helpers.php',
     );
 
@@ -123,6 +123,7 @@ class ComposerStaticInit044026184457c9850706b2f68d6e9c44
         'N' => 
         array (
             'NunoMaduro\\Collision\\' => 21,
+            'Nsavinov\\NovaPercentField\\' => 26,
             'Nikaia\\Rating\\' => 14,
         ),
         'M' => 
@@ -168,6 +169,7 @@ class ComposerStaticInit044026184457c9850706b2f68d6e9c44
         ),
         'E' => 
         array (
+            'Epartment\\NovaDependencyContainer\\' => 34,
             'Egulias\\EmailValidator\\' => 23,
         ),
         'D' => 
@@ -406,6 +408,10 @@ class ComposerStaticInit044026184457c9850706b2f68d6e9c44
         array (
             0 => __DIR__ . '/..' . '/nunomaduro/collision/src',
         ),
+        'Nsavinov\\NovaPercentField\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nsavinov/nova-percent-field/src',
+        ),
         'Nikaia\\Rating\\' => 
         array (
             0 => __DIR__ . '/..' . '/nikaia/nova-rating-field/src',
@@ -514,6 +520,10 @@ class ComposerStaticInit044026184457c9850706b2f68d6e9c44
         'Facade\\FlareClient\\' => 
         array (
             0 => __DIR__ . '/..' . '/facade/flare-client-php/src',
+        ),
+        'Epartment\\NovaDependencyContainer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/epartment/nova-dependency-container/src',
         ),
         'Egulias\\EmailValidator\\' => 
         array (
@@ -637,12 +647,19 @@ class ComposerStaticInit044026184457c9850706b2f68d6e9c44
         'App\\Models\\Setting' => __DIR__ . '/../..' . '/app/Models/Setting.php',
         'App\\Models\\Sub_category' => __DIR__ . '/../..' . '/app/Models/Sub_category.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
+        'App\\Nova\\About' => __DIR__ . '/../..' . '/app/Nova/About.php',
         'App\\Nova\\Attribut' => __DIR__ . '/../..' . '/app/Nova/Attribut.php',
         'App\\Nova\\Category' => __DIR__ . '/../..' . '/app/Nova/Category.php',
+        'App\\Nova\\Policy' => __DIR__ . '/../..' . '/app/Nova/Policy.php',
         'App\\Nova\\Product' => __DIR__ . '/../..' . '/app/Nova/Product.php',
         'App\\Nova\\Resource' => __DIR__ . '/../..' . '/app/Nova/Resource.php',
         'App\\Nova\\Review' => __DIR__ . '/../..' . '/app/Nova/Review.php',
+        'App\\Nova\\Setting' => __DIR__ . '/../..' . '/app/Nova/Setting.php',
+        'App\\Nova\\SubCategory' => __DIR__ . '/../..' . '/app/Nova/SubCategory.php',
         'App\\Nova\\User' => __DIR__ . '/../..' . '/app/Nova/User.php',
+        'App\\Policies\\AboutPolicy' => __DIR__ . '/../..' . '/app/Policies/AboutPolicy.php',
+        'App\\Policies\\PolicyPolicy' => __DIR__ . '/../..' . '/app/Policies/PolicyPolicy.php',
+        'App\\Policies\\ReviewPolicy' => __DIR__ . '/../..' . '/app/Policies/ReviewPolicy.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
@@ -1319,6 +1336,13 @@ class ComposerStaticInit044026184457c9850706b2f68d6e9c44
         'Egulias\\EmailValidator\\Warning\\QuotedString' => __DIR__ . '/..' . '/egulias/email-validator/src/Warning/QuotedString.php',
         'Egulias\\EmailValidator\\Warning\\TLD' => __DIR__ . '/..' . '/egulias/email-validator/src/Warning/TLD.php',
         'Egulias\\EmailValidator\\Warning\\Warning' => __DIR__ . '/..' . '/egulias/email-validator/src/Warning/Warning.php',
+        'Epartment\\NovaDependencyContainer\\ActionHasDependencies' => __DIR__ . '/..' . '/epartment/nova-dependency-container/src/ActionHasDependencies.php',
+        'Epartment\\NovaDependencyContainer\\FieldServiceProvider' => __DIR__ . '/..' . '/epartment/nova-dependency-container/src/FieldServiceProvider.php',
+        'Epartment\\NovaDependencyContainer\\HasChildFields' => __DIR__ . '/..' . '/epartment/nova-dependency-container/src/HasChildFields.php',
+        'Epartment\\NovaDependencyContainer\\HasDependencies' => __DIR__ . '/..' . '/epartment/nova-dependency-container/src/HasDependencies.php',
+        'Epartment\\NovaDependencyContainer\\Http\\Controllers\\ActionController' => __DIR__ . '/..' . '/epartment/nova-dependency-container/src/Http/Controllers/ActionController.php',
+        'Epartment\\NovaDependencyContainer\\Http\\Requests\\ActionRequest' => __DIR__ . '/..' . '/epartment/nova-dependency-container/src/Http/Requests/ActionRequest.php',
+        'Epartment\\NovaDependencyContainer\\NovaDependencyContainer' => __DIR__ . '/..' . '/epartment/nova-dependency-container/src/NovaDependencyContainer.php',
         'Facade\\FlareClient\\Api' => __DIR__ . '/..' . '/facade/flare-client-php/src/Api.php',
         'Facade\\FlareClient\\Concerns\\HasContext' => __DIR__ . '/..' . '/facade/flare-client-php/src/Concerns/HasContext.php',
         'Facade\\FlareClient\\Concerns\\UsesTime' => __DIR__ . '/..' . '/facade/flare-client-php/src/Concerns/UsesTime.php',
@@ -4146,6 +4170,8 @@ class ComposerStaticInit044026184457c9850706b2f68d6e9c44
         'Nikaia\\Rating\\Rating' => __DIR__ . '/..' . '/nikaia/nova-rating-field/src/Rating.php',
         'Nikaia\\Rating\\RatingServiceProvider' => __DIR__ . '/..' . '/nikaia/nova-rating-field/src/RatingServiceProvider.php',
         'Normalizer' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php',
+        'Nsavinov\\NovaPercentField\\FieldServiceProvider' => __DIR__ . '/..' . '/nsavinov/nova-percent-field/src/FieldServiceProvider.php',
+        'Nsavinov\\NovaPercentField\\Percent' => __DIR__ . '/..' . '/nsavinov/nova-percent-field/src/Percent.php',
         'NumberFormatter' => __DIR__ . '/..' . '/symfony/intl/Resources/stubs/NumberFormatter.php',
         'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider' => __DIR__ . '/..' . '/nunomaduro/collision/src/Adapters/Laravel/CollisionServiceProvider.php',
         'NunoMaduro\\Collision\\Adapters\\Laravel\\Commands\\TestCommand' => __DIR__ . '/..' . '/nunomaduro/collision/src/Adapters/Laravel/Commands/TestCommand.php',
